112ae98c74d5c163fd03cf44639d04d2f5a00955
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-06-07 08:25:25 -0300

    feat: implement interface IHashComparer

--- CodeGo.Infrastructure/Authentication/HashHandler.cs
+++ CodeGo.Infrastructure/Authentication/HashHandler.cs
@@ -5,8 +5,13 @@ namespace CodeGo.Infrastructure.Authentication;
 
 using BC = BCrypt.Net.BCrypt;
 
-public class HashHandler : IHashGenerator
+public class HashHandler : IHashGenerator, IHashComparer
 {
+    public bool Compare(string plainText, string hash)
+    {
+        return BC.Verify(plainText, hash);
+    }
+
     public string GenerateHash(string plainText)
     {
         return BC.HashPassword(plainText);
