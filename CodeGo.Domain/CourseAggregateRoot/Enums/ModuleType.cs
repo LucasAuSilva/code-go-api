a1f9dea601b6cd3986720752b17533806499a969
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-06-09 17:52:39 -0300

    fix: change properties to correct type

--- CodeGo.Domain/CourseAggregateRoot/Enums/ModuleType.cs
+++ CodeGo.Domain/CourseAggregateRoot/Enums/ModuleType.cs
@@ -5,8 +5,8 @@ namespace CodeGo.Domain.CourseAggregateRoot.Enums;
 
 public sealed class ModuleType : SmartEnum<ModuleType>
 {
-    public static readonly Language Skill = new(nameof(Skill), 1);
-    public static readonly Language Test = new(nameof(Test), 2);
+    public static readonly ModuleType Skill = new(nameof(Skill), 1);
+    public static readonly ModuleType Test = new(nameof(Test), 2);
 
     public ModuleType(string name, int value) : base(name, value)
     {}
