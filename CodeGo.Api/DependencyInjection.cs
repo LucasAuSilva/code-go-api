43d3a664340127491e0bbd4e76386864531ccb3a
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-06-13 02:30:57 -0300

    fix: make cors work

--- CodeGo.Api/DependencyInjection.cs
+++ CodeGo.Api/DependencyInjection.cs
@@ -17,6 +17,7 @@ public static class DependencyInjection
         services.AddControllers();
         services.AddSingleton<ProblemDetailsFactory, CodeGoProblemDetailsFactory>();
         services.AddScoped<IMapper, ServiceMapper>();
+        services.AddCors();
         return services;
     }
 }
