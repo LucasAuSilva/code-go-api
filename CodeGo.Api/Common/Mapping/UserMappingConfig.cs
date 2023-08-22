544e3d32f432acaae50893add3c572bf280d23dd
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-08-22 20:08:32 -0300

    fix: add mapping for level

--- CodeGo.Api/Common/Mapping/UserMappingConfig.cs
+++ CodeGo.Api/Common/Mapping/UserMappingConfig.cs
@@ -27,6 +27,7 @@ public class UserMappingConfig : IRegister
             .Map(dest => dest.Id, src => src.Id.Value.ToString())
             .Map(dest => dest.StreakCount, src => src.DayStreak.StreakCount)
             .Map(dest => dest.ExperiencePoints, src => src.Experience.Points)
+            .Map(dest => dest.Level, src => src.Level.Value.ToString())
             .Map(
                 dest => dest.CourseIds,
                 src => src.CourseIds.AsEnumerable().Select(id => id.Value.ToString()).ToList());
