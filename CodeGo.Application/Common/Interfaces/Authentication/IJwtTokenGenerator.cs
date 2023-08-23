97d7e46c7004ac548160e0dff87583ab4a64a3b3
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-08-23 11:20:45 -0300

    refactor: change JwtTokenGenerator to get all user info

--- CodeGo.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
+++ CodeGo.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -1,7 +1,9 @@
 
+using CodeGo.Domain.UserAggregateRoot;
+
 namespace CodeGo.Application.Common.Interfaces.Authentication;
 
 public interface IJwtTokenGenerator
 {
-    string GenerateToken(Guid userId, string firstName, string lastName);
+    string GenerateToken(User user);
 }
