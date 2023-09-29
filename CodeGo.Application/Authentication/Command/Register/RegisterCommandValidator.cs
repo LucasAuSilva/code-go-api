bd12f812e7da03a65087d63cb0b9c6ebab3748c1
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-09-29 14:29:18 -0300

    feat: create validator for RegisterCommand

---
+++ CodeGo.Application/Authentication/Command/Register/RegisterCommandValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+namespace CodeGo.Application.Authentication.Command.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
