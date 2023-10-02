756ccaabdf7af3fc6fa6cc51f0ffd46883454fae
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-10-02 15:55:58 -0300

    feat: validation for ListFriendRequestQuery

--- CodeGo.Application/Users/Queries/ListFriendsRequests/ListFriendsRequestsQuery.cs
+++ CodeGo.Application/Users/Queries/ListFriendsRequests/ListFriendsRequestsQuery.cs
@@ -6,6 +6,7 @@ using MediatR;
 namespace CodeGo.Application.Users.Queries.ListFriendsRequests;
 
 public record ListFriendsRequestsQuery(
+    string LoggedUserId,
     string UserId,
     int Status
 ) : IRequest<ErrorOr<List<FriendshipRequest>>>;
