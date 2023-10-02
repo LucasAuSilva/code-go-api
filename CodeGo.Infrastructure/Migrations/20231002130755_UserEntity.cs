c6a790c4635468a0717da5892268dc009311135b
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-10-02 10:12:43 -0300

    featL migration for User creation on DB

---
+++ CodeGo.Infrastructure/Migrations/20231002130755_UserEntity.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CodeGo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "character varying(254)", maxLength: 254, nullable: false),
+                    Password = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    Visibility = table.Column<int>(type: "integer", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "text", nullable: true),
+                    Bio = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "friendship_requests",
+                columns: table => new
+                {
+                    FriendshipRequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequesterId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequesterEmail = table.Column<string>(type: "character varying(254)", maxLength: 254, nullable: false),
+                    RequesterPhoto = table.Column<string>(type: "text", nullable: true),
+                    Message = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_friendship_requests", x => new { x.FriendshipRequestId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_friendship_requests_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userBlockedIds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BlockedId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userBlockedIds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_userBlockedIds_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userCourseIds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userCourseIds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_userCourseIds_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userFriendIds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FriendId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userFriendIds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_userFriendIds_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_friendship_requests_UserId",
+                table: "friendship_requests",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userBlockedIds_UserId",
+                table: "userBlockedIds",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userCourseIds_UserId",
+                table: "userCourseIds",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userFriendIds_UserId",
+                table: "userFriendIds",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "friendship_requests");
+
+            migrationBuilder.DropTable(
+                name: "userBlockedIds");
+
+            migrationBuilder.DropTable(
+                name: "userCourseIds");
+
+            migrationBuilder.DropTable(
+                name: "userFriendIds");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
