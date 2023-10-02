532245ef85673fa8e154cf494a7cb75bc5390f6d
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-10-02 14:06:20 -0300

    refactor: change UserRepository implementation to dbContext

--- CodeGo.Infrastructure/Persistance/Repositories/UserRepository.cs
+++ CodeGo.Infrastructure/Persistance/Repositories/UserRepository.cs
@@ -2,35 +2,38 @@
 using CodeGo.Application.Common.Interfaces.Persistance;
 using CodeGo.Domain.UserAggregateRoot;
 using CodeGo.Domain.UserAggregateRoot.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeGo.Infrastructure.Persistance.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    private static readonly List<User> _users = new();
+    private readonly CodeGoDbContext _dbContext;
+
+    public UserRepository(CodeGoDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 
     public async Task Add(User user)
     {
-        await Task.CompletedTask;
-        _users.Add(user);
+        await _dbContext.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
     }
 
     public async Task<User?> FindByEmail(string email)
     {
-        await Task.CompletedTask;
-        return _users.Find(user => user.Email == email);
+        return await _dbContext.Users.FirstOrDefaultAsync(user => user.Email == email); 
     }
 
     public async Task<User?> FindById(UserId id)
     {
-        await Task.CompletedTask;
-        return _users.Find(user => user.Id == id);
+        return await _dbContext.Users.FirstOrDefaultAsync(user => user.Id == id); 
     }
 
     public async Task Update(User user)
     {
-        await Task.CompletedTask;
-        _users.Remove(user);
-        _users.Add(user);
+        _dbContext.Update(user);
+        await _dbContext.SaveChangesAsync();
     }
 }
