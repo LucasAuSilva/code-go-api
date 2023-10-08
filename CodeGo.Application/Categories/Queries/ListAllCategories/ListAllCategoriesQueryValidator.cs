da23020ab274cb42a63b99ef16b3a085c49f1e75
LucasAuSilva <silvaaugustolucas@gmail.com>
2023-10-08 17:41:29 -0300

    feat: new request for ListAllCategories

--- CodeGo.Application/Categories/Queries/ListAllCategories/ListAllCategoriesQueryValidator.cs
+++ CodeGo.Application/Categories/Queries/ListAllCategories/ListAllCategoriesQueryValidator.cs
@@ -6,11 +6,5 @@ namespace CodeGo.Application.Categories.Queries.ListAllCategories;
 public class ListAllCategoriesQueryValidator : AbstractValidator<ListAllCategoriesQuery>
 {
     public ListAllCategoriesQueryValidator()
-    {
-        RuleFor(x => x.Page)
-            .NotEmpty();
-        RuleFor(x => x.PageSize)
-            .NotEmpty()
-            .LessThanOrEqualTo(20);
-    }
+    {}
 }
